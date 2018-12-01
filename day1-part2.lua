a5d2dca5b2b729b02614aa7bd43cde1b5a722dc8
Robert Helmer <rhelmer@rhelmer.org>
2018-11-30 22:35:27 -0800

    day 1 part 2

---
+++ day1-part2.lua
@@ -0,0 +1,19 @@
+frequency = 0
+seen = {}
+
+file = io.open("day1-input.txt")
+
+while true do
+    for line in file:lines() do
+        delta = tonumber(line)
+        frequency = frequency + delta
+        if seen[frequency] then
+            print(frequency)
+            return
+        end
+        seen[frequency] = true
+    end
+    file:seek("set", 0)
+end
+
+file:close()
