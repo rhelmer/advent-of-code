003f359cf030351fd106c483fb250c09072e2931
Robert Helmer <rhelmer@rhelmer.org>
2018-12-02 00:29:33 -0800

    part 1 done

--- day2/part1.lua
+++ day2/part1.lua
@@ -13,13 +13,36 @@ function tally(t)
     return count
 end
 
+local num_twos = 0
+local num_threes = 0
+
 for line in io.lines("input.txt") do
     local t = {}
     local letterCount = tally(t)
     for letter in line:gmatch("%a") do
         letterCount(letter)
     end
+
+    local twos = {}
+    local threes = {}
+
     for k, v in pairs(letterCount()) do
-        print(line, k, v)
+        if v == 2 then
+            twos[line] = true
+        elseif v == 3 then
+            threes[line] = true
+        end
+    end
+
+    -- lua doesn't seem to have an O(1) way to get the length of non-numeric
+    -- tables.
+
+    for k, v in pairs(twos) do
+        num_twos = num_twos + 1
+    end
+    for k, v in pairs(threes) do
+        num_threes = num_threes + 1
     end
 end
+
+print(num_twos * num_threes)
